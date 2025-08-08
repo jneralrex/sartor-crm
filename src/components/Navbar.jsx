1916cd58b1401ba88b2d77ae8fc323dd47e9c8b8
USER <jneralrex@gmail.com>
2025-08-08 12:25:27 +0100

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,20 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center md:py-6 md:px-10 bg-white shadow-md font-[sfpro]">
 <img src={logo} alt="" srcset="" />      
-<div className="flex items-center md:space-x-6">
+<div className="flex items-center space-x-2 md:space-x-6">
 
-      <ul className="flex md:space-x-8 text-gray-700 text-[9px] md:text-sm font-medium">
+      <ul className="flex space-x-1 md:space-x-8 text-gray-700 text-[9px] md:text-sm font-medium">
         <li><a href="#">About Us</a></li>
         <li><a href="#">Our Services ▾</a></li>
         <li><a href="#">Help Center</a></li>
       </ul>
-      <button className="bg-blue-800 text-white md:px-5 md:py-2 rounded-md text-sm hover:bg-blue-900">
+      <button className="bg-blue-800 text-white px-2 py-1 md:px-5 md:py-2 rounded-md text-[9px] md:text-sm hover:bg-blue-900">
         Book a Consultation
       </button>
       </div>
     </nav>
   );
 };
+
 
 export default Navbar;
